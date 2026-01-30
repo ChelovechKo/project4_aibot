77ec8b96bd36739087b7887811efd7b07abc67b1
zholudeva.e <ekaterinaadv@gmail.com>
2026-01-30 12:01:38 +0200

    fixed save secret

--- app/config.py
+++ app/config.py
@@ -1,22 +1,22 @@
 from typing import Optional
-
+import os
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+psycopg://postgres:postgres@postgres:5432/aibot"
-    REDIS_URL: str = "redis://redis:6379/0"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:postgres@postgres:5432/aibot")
+    REDIS_URL: str = os.getenv("REDIS_URL", "redis://redis:6379/0")
 
-    TELEGRAM_API_ID: Optional[int] = 23742505
-    TELEGRAM_API_HASH: Optional[str] = '3bf7aca6da2c4d1205724de09bf860b4'
-    TELEGRAM_SESSION_NAME: str = "aibot_session"
-    TELEGRAM_CHANNEL_USERNAME: Optional[str] = 'test_pyRush_aibot'
+    TELEGRAM_API_ID: Optional[int] = os.getenv("TELEGRAM_API_ID", 23742505)
+    TELEGRAM_API_HASH: Optional[str] = os.getenv("TELEGRAM_API_HASH", "")
+    TELEGRAM_SESSION_NAME: str = os.getenv("TELEGRAM_SESSION_NAME", "aibot_session")
+    TELEGRAM_CHANNEL_USERNAME: Optional[str] = os.getenv("TELEGRAM_CHANNEL_USERNAME", "")
 
-    OPENAI_API_KEY: Optional[str] = 'REMOVED'
+    OPENAI_API_KEY: Optional[str] = os.getenv("OPENAI_API_KEY", "")
     OPENAI_MODEL: str = "gpt-40-mini"
 
-    CELERY_BROKER_URL: str = "redis://redis:6379/0"
-    CELERY_RESULT_BACKEND: str = "redis://redis:6379/1"
+    CELERY_BROKER_URL: str = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
+    CELERY_RESULT_BACKEND: str = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/1")
 
     PARSE_INTERVAL_MINUTES: int = 30
     GENERATE_INTERVAL_MINUTES: int = 30
